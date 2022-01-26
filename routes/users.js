0e423014bb2917e6e79ba0957809adfe61d30ef5
Fawad Shahzada <fawadshahzada@gmail.com>
2022-01-26 22:29:04 +0500

    changes

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,7 @@ const { forwardAuthenticated } = require('../config/auth');
 // Login Page
 router.get('/login', forwardAuthenticated, (req, res) => res.render('login'));
 
+
 // Register Page
 router.get('/register', forwardAuthenticated, (req, res) => res.render('register'));
 
